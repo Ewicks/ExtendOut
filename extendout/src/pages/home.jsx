e494135cc860a10cf9ba8ddfad254a7841972058
ethan <ethanowicks9@gmail.com>
2024-01-26 22:55:08 +0000

    Add images

--- extendout/src/pages/home.jsx
+++ extendout/src/pages/home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Homeimage from '../assets/images/home-img.png';
+import Kitchen from '../assets/images/kitchen.png';
+import Kitchentwo from '../assets/images/kitchentwo.png';
 import '../../src/App.css';
 
 
@@ -10,6 +12,20 @@ const Home = () => {
       <div className="center">
         <h1 className='home-title font-bold underline'>Testing text center</h1>
       </div>
+      <div className="container">
+      <div className="columns-2">
+          <h1 class='w-full flex justify-center align-middle'>hi</h1>
+          <img src={Kitchen} class='kitchen-img' alt="" />
+       
+        </div>
+        <div className="columns-2">
+          <img src={Kitchentwo} class='kitchen-img' alt="" />
+          <h1 class='w-full flex justify-center align-middle'>hi</h1>
+       
+        </div>
+
+      </div>
+       
     </div>
 
   );
