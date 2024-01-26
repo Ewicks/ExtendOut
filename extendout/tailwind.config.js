fc397540322b7ea6319858cf92fac1358c2736bc
ethan <ethanowicks9@gmail.com>
2024-01-26 22:16:37 +0000

    Connect tailwind css

---
+++ extendout/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    // ... other Tailwind CSS configurations
+  },
+  plugins: [
+    // ... other Tailwind CSS plugins
+  ],
+}
