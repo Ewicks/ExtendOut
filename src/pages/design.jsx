e2b19e3da02ee352874db9ca9bf02577e87ac5cf
ethan <ethanowicks9@gmail.com>
2024-01-28 23:37:58 +0000

    Add fixes

--- src/pages/design.jsx
+++ src/pages/design.jsx
@@ -65,7 +65,7 @@ function Design() {
 
         </div>
         <div className="col d-flex justify-content-start align-items-center">
-          <a className='btn contact-btn' href="">Contact Us</a>
+          <a className='btn contact-btn' href="#">Contact Us</a>
 
         </div>
       </div>
